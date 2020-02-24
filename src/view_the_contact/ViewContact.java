5d66fef71f086e35a073a00bc5cf66a2490d766a
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-02-24 22:18:18 +0530

    project done

--- src/view_the_contact/ViewContact.java
+++ src/view_the_contact/ViewContact.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 
 public class ViewContact {
     public void viewDetails(Person p) {
-        System.out.println("-------- * -------- * -------- * --------");
+        System.out.println("--------*--------*--------*--------");
         System.out.println("First Name: " + p.getFirstName());
         System.out.println("Last Name: " + p.getLastName());
         String s = "" + p.getContactNumbers();
         System.out.println("Contact Number(s): " + s.substring(1, s.length() - 1));
         System.out.println("Email address: " + p.getEmailAddress());
-        System.out.println("-------- * -------- * -------- * --------");
+        System.out.println("--------*--------*--------*--------");
     }
 
     public void viewContact(ArrayList<Person> obj) {
